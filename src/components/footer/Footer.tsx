c250c79bf0bc8c8215fb012570a8c08d58dacdcb
Vinicius Pessan <136939544+ViniPessan@users.noreply.github.com>
2025-01-23 21:46:28 -0300

    ajustando a descrições de projetos

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -35,7 +35,7 @@ export default function Footer() {
         <div className={styles.emailAndPhoneSection}>
           <div className={`flex flex-col justify-center`}>
             <div className='flex items-center'>
-             <img src="/email.png" className={styles.footerImg3} alt="404" />
+             <img src="/gmail.png" className={styles.footerImg3} alt="404" />
              <p className='px-2'>vini.pessan1@gmail.com</p>
             </div>
             <div className='flex pt-1 items-center'> 
